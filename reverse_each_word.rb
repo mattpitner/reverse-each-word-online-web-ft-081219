c114972fb605484be57157e68f00fca33da323da
Matt Pitner <mattpitner09@gmail.com>
2019-08-16 02:11:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,11 +17,11 @@ sentence1 = "Hello there, and how are you?"
      
 # end
  
- def reverse_each_word(sentence1)
+def reverse_each_word(sentence1)
     sen1 = sentence1.split(" ")
-    
+
     arr = sen1.collect {|x| x.reverse}
-    return arr 
+    return arr.join(" ")
 end
 
 p reverse_each_word(sentence1)
