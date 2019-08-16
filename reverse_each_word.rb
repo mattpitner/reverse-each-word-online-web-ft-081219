2054c2be269a66609a6df509a24a8576355a8029
Matt Pitner <mattpitner09@gmail.com>
2019-08-16 02:10:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,28 @@
 sentence1 = "Hello there, and how are you?" 
 
-def reverse_each_word(sentence1)
-    arr=[]
-   sen1 =  sentence1.split(" ") 
-    sen1.each do |s|
-      arr.push s.reverse  
-    end
-      return  arr.join(" ")
-    return arr
-  end 
-   p reverse_each_word(sentence1)
+# def reverse_each_word(sentence1)
+#     arr=[]
+#   sen1 =  sentence1.split(" ") 
+#     sen1.each do |s|
+#       arr.push s.reverse  
+#     end
+#       return  arr.join(" ")
+#     return arr
+#   end 
+#   p reverse_each_word(sentence1)
    
    
-   def reverse_each_word(sentence1)
+#   def reverse_each_word(sentence1)
      
      
- end
+# end
+ 
+ def reverse_each_word(sentence1)
+    sen1 = sentence1.split(" ")
+    
+    arr = sen1.collect {|x| x.reverse}
+    return arr 
+end
+
+p reverse_each_word(sentence1)
      
